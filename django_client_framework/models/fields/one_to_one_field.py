33e9bb900aa2dcb80934a73fa453f146c48959d7
Yu Li <ylilarry@gmail.com>
2021-09-15 23:25:18 -0400

    Fix UniqueForeignKey

--- django_client_framework/models/fields/one_to_one_field.py
+++ django_client_framework/models/fields/one_to_one_field.py
@@ -1,8 +1,12 @@
-from django.db import models as m
-from django.db.models.fields.related import ReverseOneToOneDescriptor
+from django.db.models.fields.related import ReverseOneToOneDescriptor, OneToOneField
+from django.core.exceptions import ObjectDoesNotExist
+from typing import TypeVar, Generic
+from ..abstract import DCFModel
+
+T = TypeVar("T", bound=DCFModel)
 
 
-class UniqueForeignKey(m.OneToOneField):
+class UniqueForeignKey(Generic[T], OneToOneField[T]):
     """
     This class fix django's OneToOneField's historical problem, where accessing
     through the reverse relation when the object does not exist would raise an
@@ -13,7 +17,7 @@ class UniqueForeignKey(m.OneToOneField):
         def __get__(self, *args, **kwargs):
             try:
                 return super().__get__(*args, **kwargs)
-            except m.ObjectDoesNotExist:
+            except ObjectDoesNotExist:
                 return None
 
     related_accessor_class = FixReverseOneToOneDescriptor
