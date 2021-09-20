3f65ab3b0c75518643e73f15c6e0eb57da93d090
Yu Li <ylilarry@gmail.com>
2021-09-20 00:04:08 -0400

    fix type

--- django_client_framework/permissions/__init__.py
+++ django_client_framework/permissions/__init__.py
@@ -1,2 +1,3 @@
 from .default_users import *
+from .default_groups import *
 from .site_permission import *
--- django_client_framework/serializers/fields/serializer_method_field.py
+++ django_client_framework/serializers/fields/serializer_method_field.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers as s
+from rest_framework.fields import Field
 
 
 class TypedSerializerMethodField(s.SerializerMethodField):
-    def __init__(self, *args, **kwargs):
-        self.type = kwargs.pop("type")
+    def __init__(self, type: Field, *args, **kwargs):
+        self.type = type
         super().__init__(*args, **kwargs)
