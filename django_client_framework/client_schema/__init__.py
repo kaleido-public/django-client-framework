3cc650ec36640c439be8123311164cd3ad8fd36b
Yu Li <ylilarry@gmail.com>
2021-12-24 21:21:52 -0500

    Remove client schema code

--- django_client_framework/client_schema/__init__.py
+++
@@ -1,4 +0,0 @@
-# import the converters to register them
-from . import converters  # noqa
-from .convert import to_jsonschema  # noqa
-from .fields import JSONSchemaField, SerializerJSONField  # noqa
