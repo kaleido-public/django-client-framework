6a6a20503362c61861d635c57bd41b4ffd97397e
Yu Li <ylilarry@gmail.com>
2022-01-29 04:45:03 -0500

    add monkeypatch_pk_field

--- django_client_framework/settings.py
+++ django_client_framework/settings.py
@@ -1,4 +1,5 @@
-from typing import Any, List
+import uuid
+from typing import *
 
 from django.db.models.fields.related import OneToOneField
 
@@ -33,6 +34,33 @@ def monkeypatch_abstract_generic() -> None:
     ModelState.render = _new  # type: ignore
 
 
+def monkeypatch_pk_field(app_name: str) -> None:
+    """Forces all generated models for ManyToManyFields to use UUID primary key.
+    This is needed until Django natively supports using UUID pk field."""
+    from django.db.models import UUIDField
+    from django.db.models.options import Options
+
+    original = None  # type: ignore
+
+    class UUIDPKField(UUIDField):
+        def __init__(self, *args: Any, **kwargs: Any) -> None:
+            kwargs.pop("auto_created", None)
+            kwargs["default"] = uuid.uuid4
+            super().__init__(*args, **kwargs)
+
+    def patch_get_default_pk_class(
+        self: Any, *args: Any, **kwargs: Any
+    ) -> Type[UUIDPKField]:
+        if self.db_table.startswith(f"{app_name}_"):
+            return UUIDPKField
+        else:
+            return original(self, *args, **kwargs)  # type: ignore
+
+    if Options._get_default_pk_class is not patch_get_default_pk_class:  # type:ignore
+        original = Options._get_default_pk_class  # type: ignore
+        Options._get_default_pk_class = patch_get_default_pk_class  # type:ignore
+
+
 def install(
     INSTALLED_APPS: List[str],
     REST_FRAMEWORK: Any,
@@ -45,7 +73,6 @@ def install(
     ]
     MIDDLEWARE += [
         "django_client_framework.exceptions.handlers.ConvertAPIExceptionToJsonResponse",
-        "django_currentuser.middleware.ThreadLocalUserMiddleware",
     ]
     REST_FRAMEWORK.update(
         {
@@ -53,9 +80,8 @@ def install(
             "NON_FIELD_ERRORS_KEY": "general_errors",
         }
     )
-    AUTHENTICATION_BACKENDS += [
-        "django.contrib.auth.backends.ModelBackend",
-    ]
+    AUTHENTICATION_BACKENDS += []
 
     monkeypatch_abstract_generic()
     monkeypatch_djangotypes()
+    monkeypatch_pk_field("django_client_framework")
